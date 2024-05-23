ebe255346a58f50f9a322603b1eb65cadd3d45c0
Function <67085909+Function1790@users.noreply.github.com>
2024-05-23 19:35:12 +0900

    주석 추가

--- datastructure/Tree.js
+++ datastructure/Tree.js
@@ -1,5 +1,3 @@
-floor()
-
 //Base
 class Tree {
     constructor(value = null, children = [null]) {
@@ -7,6 +5,12 @@ class Tree {
         this.children = this.children
     }
 }
+const tree = new Tree(1, [2, new Tree(3, [4, 5])])
+    // 구조 모양 
+    //     1
+    //   2   3
+    //      4 5
+
 
 //Binary Tree
 function getIndexOfParent(currentIndex) {
@@ -18,13 +22,17 @@ class BinaryTree {
         this.arr = arr
     }
     getParent(currentIndex) {
-        return this.arr[getIndexOfParent(currentIndex)]
-    }
+            // 부모 노드의 인덱스 = 내림((자신의 인덱스)/2)
+            // getIndexOfParent는 자신의 인덱스로 부터 부모의 인덱스를 불러오는 함수
+            return this.arr[getIndexOfParent(currentIndex)] // 부모의 값 반환
+        }
+        // 이진트리에 값 추가하기
     push(item) {
-        this.arr.push(item)
-    }
-    /** [left] dir=0  [right] dir=1*/
+            this.arr.push(item)
+        }
+        /** 왼쪽 노드: dir=0 | 오른쪽 노드: dir=1*/
     getChild(currentIndex, dir = 0) {
-        return this.arr[currentIndex * 2 + dir]
+        // 자식노드의 인덱스 = 2i or 2i+1
+        return this.arr[currentIndex * 2 + dir] // 자식노드의 값 반환
     }
 }
